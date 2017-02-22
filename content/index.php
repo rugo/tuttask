021ebdbfb003bdf209ad9e83d4a07089f2cdecbc
rugo <rg@ht11.org>
2017-02-22 11:32:10 +0100

    Updated service

--- content/index.php
+++ content/index.php
@@ -1,7 +1,22 @@
+<html><head><title>Mailbox</title></head><body>
 <?php
 $fn = "welcome";
+$path = "letters";
 if ($_GET) {
-        $fn = $_GET['fn']; 
+    $fn = $_GET['fn']; 
 }
-echo file_get_contents("letters/${fn}.txt");
+// Scan for letters
+$files = scandir($path);
+// Remove . and ..
+$files = array_diff(scandir($path), array('.', '..'));
+
+// List letters
+foreach ($files as $letter) {
+    // Without extension
+    $letter = basename($letter, "txt");
+    echo "<a href='?fn=${letter}'>${letter}</a><br/>";
+}
+echo "Here is your message:<br/>";
+echo file_get_contents("${path}/${fn}.txt");
 ?>
+</body></html>
